d63028ec0657ccc88e5f00cf65ddae574695f964
Meagan Campbell <meagancampbell@users.noreply.github.com>
2021-07-08 20:42:13 +0000

    screwed this one up. maybe go back to previous commit after this

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -52,64 +52,90 @@ function initialPrompt() {
   return word;
 };
 
-let simpleScore = function() {
+// let simpleScore = function() {
+//   let points = 0;
+//   //word = word.toUpperCase();
+//   for (let i=0; i < word.length; i++) {
+//     points++;
+//   }
+//   return points;
+// };
+
+// let bonusTally = 0;
+// const vowelBonus = ['A', 'E', 'I', 'O', 'U'];
+
+// let vowelBonusScore = function() {
+//   //TODO: case insensative
+//   word = "word";
+//   //word = word.toUpperCase(); 
+//   for (let i = 0; i < word.length; i++) {
+//   //if vowel
+//   if (word[i] === vowelBonus[0] || word[i] === vowelBonus[1] || word[i] === vowelBonus[2]|| word[i] === vowelBonus[3] || word[i] === vowelBonus[4]) {
+//     bonusTally = bonusTally + 1;
+//     }else{
+//     points = points + 1;
+//       } 
+//     }
+//   let bonuses = (bonusTally * 3);
+//   points += points + bonuses;
+//   return vowelBonusScore;
+// }
+
+// let scrabbleScore = function() {
+//     let points = 0;
+//     for (let i = 0; i < word.length; i++) {
+//     points = Number(newPointStructure[word[i]]);
+//     }
+//     return points;
+// };
+
+
+
+let simpleScoreObj = {
+  "name": "Simple Score",
+  "number": "0",
+  "description": "Each letter is worth 1 point.",
+  scoringFunction: simpleScore = function() {
   let points = 0;
-  //word = word.toUpperCase();
-  for (let i=0; i < word.length; i++) {
-    points++;
-  }
-  return points;
-};
-
-let bonusTally = 0;
-const vowelBonus = ['A', 'E', 'I', 'O', 'U'];
-
-let vowelBonusScore = function() {
-  //TODO: case insensative
-  word = "word";
-  //word = word.toUpperCase(); 
   for (let i = 0; i < word.length; i++) {
-  //if vowel
-  if (word[i] === vowelBonus[0] || word[i] === vowelBonus[1] || word[i] === vowelBonus[2]|| word[i] === vowelBonus[3] || word[i] === vowelBonus[4]) {
-    bonusTally = bonusTally + 1;
-    }else{
-    points = points + 1;
-      } 
-    }
+  points++;
+          }
+      }
+}
+
+let vowelBonusScoreObj = {
+  "name": "Vowel Bonus Score",
+  "number": "1",
+  "description": "Vowels are 3 pts, Consonants are 1 pt.",
+  scoringFunction: vowelBonusScore = function() {
+  word = "word";
+  for (let i = 0; i < word.length; i++) {
+  if (word[i] === vowelBonus[0] || word[i] === vowelBonus[1] || word[i] === vowelBonus[2] || word[i] === vowelBonus[3] || word[i] === vowelBonus[4]) {
+  bonusTally = bonusTally + 1;
+  } else {
+  points = points + 1;
+  }
   let bonuses = (bonusTally * 3);
   points += points + bonuses;
   return vowelBonusScore;
+  }
+  }
 }
 
-let scrabbleScore = function() {
-    let points = 0;
-    for (let i = 0; i < word.length; i++) {
-    points = Number(newPointStructure[word[i]]);
-    }
-    return points;
+let scrabbleScoreObj = {
+  "name": "Scrabble Score",
+  "number":"2",
+  "description": "The traditional scoring algorithm.",
+  scoringFunction: scrabbleScore = function() {
+  let points = 0;
+  for (let i = 0; i < word.length; i++) {
+  points = Number(newPointStructure[word[i]]);
+  }
+  return points;
+  }
 };
 
-const scoringAlgorithms = [simpleScore(), vowelBonusScore(), scrabbleScore()];
-
-let scoringObjects= [{
-        name: "Simple Score",
-        description: "Each letter is worth 1 point.",
-        scoringFunction: simpleScore(word)
-    },
-    {
-        name: "Vowel Bonus Score",
-        description: "Vowels are 3 pts, Consonants are 1 pt.",
-        scoringFunction: vowelBonusScore(word)
-    },
-    {
-        name: "Scrabble Score",
-        description: "The traditional scoring algorithm.",
-        scoringFunction: scrabbleScore(word)
-    }
-];
-
-
-
+const scoringAlgorithms = [simpleScoreObj, vowelBonusScoreObj, scrabbleScoreObj];
 
 function scorerPrompt() {
     let simpleMethod = "0 - Simple Scoring Method: One point per character";
@@ -118,38 +144,38 @@ function scorerPrompt() {
     console.log(`Which scoring algorithm would you like to use? \n ${simpleMethod} \n ${vowelMethod} \n ${traditionalMethod} `);
     let scoringMethod = (input.question("Enter 0, 1, or 2 "));
     let number = scoringMethod;
-    console.log("scoringMethod");
-      if (number == 0) {
-      simpleScore();
+    //console.log("scoringMethod");
+    if (number == "0") {
+      return scoringAlgorithms[0].scoringFunction.simpleScore;
       } 
         // console.log("Scoring Choice: " + scoringAlgorithms[0].scoringFunction.name);
         // console.log();
         // console.log(`Is worth ${simpleScore(word, newPointStructure)} points`);
-      if (number == 1) {
-      vowelBonusScore();
+    if (number == "1") {
+      return scoringAlgorithms[0].scoringFunction.vowelBonusScore;
       } 
         // console.log("Scoring Choice: " + scoringAlgorithms[1].scoringFunction.name);
         // console.log();
         // console.log(`Is worth ${vowelBonusScore(word, newPointStructure)} points`);
 
-      if (number == 2) {
-      scrabbleScore();
+    if (number == "2") {
+      return scoringAlgorithms[0].scoringFunction.scrabbleScore;
       }
         // console.log("Scoring Choice: " + scoringAlgorithms[2].scoringFunction.name);
         // console.log();
         // console.log(`Is worth ${scrabbleScore(word, newPointStructure)} points`);
 
-      if (number > 2 || number == NaN){ 
+      if (number > "2" || number == NaN){ 
       console.log("Incorrect selection. Choose again.");
       
       }
-    console.log("Nothing is working");
+    //console.log("Nothing is working");
 };
 
 
 
 
-console.log("letter a: ", newPointStructure.a);
+console.log("letter pineapple: ", newPointStructure.word);
 console.log("letter j: ", newPointStructure.j);
 //console.log("letter z: ", newPointStructure["z"]);
 
